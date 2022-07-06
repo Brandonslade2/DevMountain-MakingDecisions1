a428d379aadc8d422397a8a540d621023e3d0300
Jay Slade <brandonslade2@gmail.com>
2022-07-05 21:02:10 -0700

    Making Decisions Problem 1-3

--- lab.js
+++ lab.js
@@ -42,7 +42,11 @@ if (amysAge > brittanisAge) {
   Using an if statement and a comparison operator, create code that will check if Amy and Brittani were born in the same year. If they were, console.log 'Amy & Brittani were born in the same year'. If they were not, console.log 'Amy & Brittani were not born in the same year'
 */
 
-//CODE HERE
+if (amysBirthYear === brittanisBirthYear) {
+  console.log('Amy and Brittani were born in the same year.')
+} else {
+  console.log('Amy and Brittani were not born in the same year.')
+}
 
 ////////// PROBLEM 4 //////////
 
