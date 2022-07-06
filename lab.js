dc65b7bd911fdfd486163d74feb39fc45735ed86
Jay Slade <brandonslade2@gmail.com>
2022-07-05 21:54:25 -0700

    Making Decisions Problems 1-11

--- lab.js
+++ lab.js
@@ -117,7 +117,10 @@ let passingScore = 7;
   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
 */
 
-//CODE HERE
+while (passingScore < 7) {
+  console.log('Your score is not high enough')
+  score += 1
+}
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -128,12 +131,20 @@ let passingScore = 7;
 
 */
 
-//CODE HERE
+var changeMyMind = true
 
+if (changeMyMind === true) {
+  changeMyMind === false
+} else {
+  changeMyMind === true
+}
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
-//CODE HERE
+if (changeMyMind != true) {
+  changeMyMind = true
+}
+console.log(changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -142,4 +153,11 @@ let passingScore = 7;
 let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+while (z > 0) {
+  console.log(z)
+  z +- 1
+  while (z > 0) {
+    z = y
+    console.log(y)
+  }
+}
