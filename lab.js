898c2fae89281b8aaa0be46d01858d3ecb9743b0
Jay Slade <brandonslade2@gmail.com>
2022-07-05 20:57:30 -0700

    Making Decisions Problem 1-2

--- lab.js
+++ lab.js
@@ -6,7 +6,13 @@
   If it is not, console log "Coding has its challenges."
 */
 
-//CODE HERE
+var lovesCode = true
+
+if (lovesCode = true) {
+  console.log('I love to code!')
+} else {
+  console.log('Coding has its challenges.')
+}
 
 // For problems 2-3 use the following lines of code:
 var amysAge = 29;
@@ -20,7 +26,15 @@ var brittanisBirthYear = 1986;
   Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
 */
 
-//CODE HERE
+if (amysAge > brittanisAge) {
+  console.log('Amy is older')
+} else {
+  if (amysAge = brittanisAge) {
+    console.log('Amy is the same age as Brittani')
+  } else {
+    console.log('Brittani is older')
+}
+}
 
 ////////// PROBLEM 3 //////////
 
